bde32f48d8b45c3598b0ff903e9ad84fb08b051e
Arnaud-BERTRAND33 <100602840+Arnaud-BERTRAND33@users.noreply.github.com>
2022-06-23 18:03:24 +0200

    Add files via upload

---
+++ Indy.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+class Indy {
+    public static void main(String[] args) {
+        String name = "La dernière croisade";
+        boolean movieSeen = true;
+        int releaseDate = 1989;
+        double note = 8.2;
+        System.out.print("Nom : " +name+ " Date : " +releaseDate+ " Note : " +note+ " Film vu : " +movieSeen);
+    }
+
+}
